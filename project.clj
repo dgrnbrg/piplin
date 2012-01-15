2e778318205a50aa599e9b07dcaf020b8351da34
David Greenberg <dsg123456789@gmail.com>
2012-01-15 16:57:01 -0500

    added fleet dependency

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject piplin "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.clojure/core.logic "0.6.7"]])
+                 [org.clojure/core.logic "0.6.7"]
+                 [fleet "0.9.5"]])
