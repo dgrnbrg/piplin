043b96b1c2a3fa78afe51f890e24e3202838ac67
David Greenberg <dsg123456789@gmail.com>
2012-01-21 12:10:28 -0500

    Some design notes on backpressure/valid signals

--- src/piplin/math.clj
+++ src/piplin/math.clj
@@ -85,7 +85,6 @@
 (defbinop bit-and 0)
 (defbinop bit-or 0)
 (defbinop bit-xor 0)
-(defbinop bit-not 0)
 
 (defmacro defbinopimpl
   "Defines implementation of a binop on a piplin kind.
@@ -125,3 +124,110 @@
 
 ;(+ ((uintm 8) 3) -4)
 ;(+ 3 ((uintm 8) 4))
+
+(comment
+  TODO:
+  - error reporting (file/lineno)
+  - enforce boundaries of uintm [0 ..< 2**n]
+  - implement other operations on uintm
+  - implement sintm, sints, uints, etc. (e type is hard)
+  - design AST
+  - implement sim function, ast, and
+    verilog module binding macro(s)
+  - implement simulation with syn/ack)
+
+(comment
+  Synthesizable fragments are either expressions or structures.
+
+  Expressions either are immediates or are values.
+
+  All expressions have types and error info.
+
+  The details of a value expression are: the name of the verilog
+  module it links to & that module's portspec, the function
+  which implements its functionality, its walkable
+  arguments, and its other arguments.
+
+  The details of an immediate expression are: the piplin
+  instance of it, and the function that converts it
+  to verilog. Instead of a piplin instance, it could instead
+  have a port usage.
+
+  The details of a structure are the inputs (themselves exprs),
+  the output/feedback regs (with exprs whose termini must be
+  immediates)
+  )
+
+(comment
+  Simulation:
+  We have a datastructure containing the value of every register
+  We run all the functions to get the new values of registers on
+  which there is data available.
+  Repeat.
+  )
+
+(comment
+  Structures serve as barriers
+  i.e. it produces a result no faster than the longest pipline in it.
+
+  Many primitives are only valid when their inputs are valid. How do
+  we communicate validity? With 
+
+  Suppose not: does it become hard to reason about how the data flows?
+  Needs a priority encoder/round robin primitive--when 2 things could
+  happen, pick one.
+
+  I am thinking now to have structural blocks serve as synchronization
+  points, and then provide the priority/round-robin bypass/merging
+  combinators.
+  )
+
+(comment
+  We can propagate metadata via a macro:
+  enforces that metadata threading points are in the same lexical scope
+
+  (let ({:keys [:pipe :key :key2]}) (metapipe {:key val :key2 val}
+                  (pipeline (exprs key) key2))
+    body...)
+  )
+
+(comment
+  I'm trying to figure out how to write code w/ valid and stall bits
+  effectively programmed. This example is for an ALU w/ an intmath
+  pipeline that computes +, -, *, and /, and a cordic processor that
+  computes sin and cos.)
+
+(defn prioritymerge [x y]
+  (if-not (= (type x) (type y))
+    (error "Can only merge data of same type")
+    ;if x is valid, returns valid x
+    ;if y is valid and x isn't, returns valid y
+    ;if x and y are invalid, returns invalid
+    (if (valid? x) x y)))
+
+(defn mkalu [data op]
+  (structural [:in data op :out results (aluresult nil)]
+    (let [intmath-op (isany? op #{:+ :- :* :/})
+          cordic-op (bit-not intmath-op)
+          ;isany? and bit-not can be used in guarded-with clauses
+          ;since they can be synthesized as combinational logic
+          ;noncombinational logic cannot be used as the guard
+          intpipe (guarded-with intmath-op
+                    (mkintmath op data))
+          cordicpipe (guarded-with cordic-op
+                       (mkcordic op data))]
+      ;merge functions do arbitration
+      (connect results
+               (priority-merge intpipe cordicpipe))
+      (comment
+        How should backpressure work? Here's my hypothesis:
+
+        If the output node is asserting stall backwards, then we
+        must not produce more results. Do this by having the stall
+        propagate through the pipeline backward. We'll need to
+        write the logic that explains when we're stalled to our
+        inputs, however.
+
+        )
+      (backpressure (or (and intmath-op (ready? intpipe))
+                        (and cordic-op (ready? cordic-pipe)))))))
