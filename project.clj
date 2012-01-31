4b44e24c0f596e725ebfb0bd9e2aa14ae36c5432
David Greenberg <dsg123456789@gmail.com>
2012-01-31 18:47:33 -0500

    Updated dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/core.logic "0.6.7"]
                  [fleet "0.9.5"]
+                 [slingshot "0.10.1"]
+                 [org.clojure/tools.macro "0.1.1"]
+                 [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/algo.generic "0.1.0"]])
