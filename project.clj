59c40f9f2068ed0bb95f77f98ee0b35d3aab9e6b
David Greenberg <dsg123456789@gmail.com>
2012-06-10 09:42:12 -0400

    Upgrade to lein2

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject piplin "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+(defproject piplin/piplin "1.0.0-SNAPSHOT" 
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojure/core.logic "0.6.7"]
                  [fleet "0.9.5"]
@@ -7,5 +6,6 @@
                  [org.clojure/tools.macro "0.1.1"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/algo.generic "0.1.0"]]
-  :dev-dependencies [[lein-marginalia "0.7.0"]
-                     [lein-pprint "1.1.1"]])
+  :min-lein-version "2.0.0"
+  :plugins [[lein-marginalia "0.7.0"] [lein-pprint "1.1.1"]]
+  :description "FIXME: write description")
